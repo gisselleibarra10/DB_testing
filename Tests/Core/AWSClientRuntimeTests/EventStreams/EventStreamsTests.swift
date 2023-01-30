9d11a4518cc01794126e48c97a2b77d96d137d4b
Ganesh Jangir <jangirg@amazon.com>
2023-01-30 14:09:49 -0800

    wip

---
+++ Tests/Core/AWSClientRuntimeTests/EventStreams/EventStreamsTests.swift
@@ -0,0 +1,13 @@
+import XCTest
+import ClientRuntime
+@testable import AWSClientRuntime
+
+class EventStreamsTests: XCTestCase {
+    func testReadMessage() {
+        let buffer: [UInt8] = [0, 0, 0, 0x0E, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F]
+        let streamReader = DataStreamReader()
+        streamReader.write(buffer: .init(bytes: buffer))
+        let message = AWSMessageDecoder.readMessage(streamReader: streamReader)
+        XCTAssertEqual(message.bytes(), [0, 0, 0, 0x0E, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E])
+    }
+}
---
+++ Tests/Services/AWSTranscribeStreamingTests/EventStreams.swift
@@ -0,0 +1,100 @@
+//
+//  EventStreams.swift
+//
+//
+//  Created by Jangir, Ganesh on 1/30/23.
+//
+
+import XCTest
+@testable import AWSTranscribeStreaming
+import ClientRuntime
+import AWSClientRuntime
+
+final class EventStreams: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() async throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        // Any test you write for XCTest can be annotated as throws and async.
+        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
+        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+
+//        let client = try TranscribeStreamingClient(region: "us-west-2")/
+//        guard let path = Bundle.module.url(forResource: "hello-kotlin-8000", withExtension: ".wav") else {
+//            XCTFail("Could not find file")
+//            return
+//        }
+
+        let path = "/Users/jangirg/Projects/Amplify/SwiftSDK/aws-sdk-swift/Tests/Services/AWSTranscribeStreamingTests/Resources/hello-kotlin-8000.wav"
+
+        let audio = FileManager.default.contents(atPath: path)!
+        let fileStream = DataStreamReader()
+        fileStream.write(buffer: .init(data: audio))
+
+        let client = try TranscribeStreamingClient(region: "us-west-2")
+        let framesPerChunk: UInt = 4096
+        let stream = AsyncThrowingStream<TranscribeStreamingClientTypes.AudioStream, Error> { continuation in
+            Task {
+                while true {
+                    let chunk = fileStream.read(maxBytes: framesPerChunk).getData()
+                    if chunk.count == 0 {
+                        break
+                    }
+                    let audioEvent =  TranscribeStreamingClientTypes.AudioStream.audioevent(.init(audioChunk: chunk))
+                    continuation.yield(audioEvent)
+                }
+            }
+        }
+        let audioStream = AsyncRequestStream<TranscribeStreamingClientTypes.AudioStream>(stream)
+        let input = StartStreamTranscriptionInput(audioStream: audioStream,
+                                                  languageCode: TranscribeStreamingClientTypes.LanguageCode.enUs,
+                                                  mediaEncoding: TranscribeStreamingClientTypes.MediaEncoding.pcm)
+        let output = try await client.startStreamTranscription(input: input)
+        var fullMessage = ""
+        for try await event in output.transcriptResultStream! {
+            switch event {
+            case .transcriptevent(let event):
+                for result in event.transcript?.results ?? [] {
+                    guard let transcript = result.alternatives?.first?.transcript else {
+                        continue
+                    }
+                    print("received TranscriptEvent: isPartial = \(result.isPartial), transcript = \(transcript)")
+                    if !result.isPartial {
+                        fullMessage.append(transcript)
+                    }
+                }
+            case .badrequestexception(_):
+                fatalError()
+            case .limitexceededexception(_):
+                fatalError()
+            case .internalfailureexception(_):
+                fatalError()
+            case .conflictexception(_):
+                fatalError()
+            case .serviceunavailableexception(_):
+                fatalError()
+            case .sdkUnknown(_):
+                fatalError()
+            }
+        }
+
+        print(fullMessage)
+
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
